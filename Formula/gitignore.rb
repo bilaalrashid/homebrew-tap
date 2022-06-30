8451043ce5edf48e2982db3198b6424a9c7d5f16
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2022-06-30 19:47:09 +0100

    Fix url of gitignore formula

--- Formula/gitignore.rb
+++ Formula/gitignore.rb
@@ -1,8 +1,7 @@
 class Gitignore < Formula
   desc "Effortlessly generate .gitignore files from the command-line"
   homepage "https://github.com/bilaalrashid/gitignore"
-  url "https://github.com/bilaalrashid/gitignore/archive/refs/tags/#{version}.tar.gz"
-  version "1.0.0"
+  url "https://github.com/bilaalrashid/gitignore/archive/refs/tags/1.0.0.tar.gz"
   sha256 "b7b1882788185020bf30b7d302836dfd9ee462d100bd3b8cc362746371a2663a"
   license "MIT"
 
