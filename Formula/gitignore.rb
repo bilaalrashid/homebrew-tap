a5304cc825afa0aaa88dade05e813ed32a8306ca
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2022-06-30 19:36:47 +0100

    Reorder definititons of gitignore formula

--- Formula/gitignore.rb
+++ Formula/gitignore.rb
@@ -1,8 +1,8 @@
 class Gitignore < Formula
   desc "Effortlessly generate .gitignore files from the command-line"
+  homepage "https://github.com/bilaalrashid/gitignore"
   version "1.0.0"
   sha256 "b7b1882788185020bf30b7d302836dfd9ee462d100bd3b8cc362746371a2663a"
-  homepage "https://github.com/bilaalrashid/gitignore"
   url "https://github.com/bilaalrashid/gitignore/archive/refs/tags/#{version}.tar.gz"
   license "MIT"
 
