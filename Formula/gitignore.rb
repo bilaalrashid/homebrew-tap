2e0991ca9d46f250dc1f79615aaace1438c52456
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-10-03 22:13:19 +0100

    Bump gitignore to v1.0.1

--- Formula/gitignore.rb
+++ Formula/gitignore.rb
@@ -1,8 +1,8 @@
 class Gitignore < Formula
-  desc "Effortlessly generate .gitignore files from the command-line"
+  desc "Effortlessly generate .gitignore files from the command line"
   homepage "https://github.com/bilaalrashid/gitignore"
-  url "https://github.com/bilaalrashid/gitignore/archive/refs/tags/1.0.0.tar.gz"
-  sha256 "b7b1882788185020bf30b7d302836dfd9ee462d100bd3b8cc362746371a2663a"
+  url "https://github.com/bilaalrashid/gitignore/archive/refs/tags/1.0.1.tar.gz"
+  sha256 "ea4a5d997930ac453b905ffb551d6011119ca62f725e9ac3d042533a21990e20"
   license "MIT"
 
   def install
