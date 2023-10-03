ce22c0d2459b5ae9c7fdc8f82776da64cab4c4f3
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-10-03 22:17:48 +0100

    Conform gitignore description to linting

--- Formula/gitignore.rb
+++ Formula/gitignore.rb
@@ -1,5 +1,5 @@
 class Gitignore < Formula
-  desc "Effortlessly generate .gitignore files from the command line"
+  desc "Effortlessly generate .gitignore files from the command-line"
   homepage "https://github.com/bilaalrashid/gitignore"
   url "https://github.com/bilaalrashid/gitignore/archive/refs/tags/1.0.1.tar.gz"
   sha256 "ea4a5d997930ac453b905ffb551d6011119ca62f725e9ac3d042533a21990e20"
